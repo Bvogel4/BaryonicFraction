f4d758ed0d05337dcc957a14d50cb1a638c3bbbb
jdvannest (macbook) <jordanvannest@gmail.com>
2021-10-14 14:57:24 -0500

    Added Plots for Observed Fb values

--- Code/Plot.py
+++ Code/Plot.py
@@ -1,7 +1,15 @@
-import imageio,os,pickle
+import argparse,imageio,os,pickle
 import numpy as np
 import matplotlib.pylab as plt
 from osxmetadata import OSXMetaData
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o","--observed",action='store_true')
+args = parser.parse_args()
+
+pre = 'Observed' if args.observed else ''
+xlower=1e-11 if args.observed else 1e-7
+tlower=7e-4 if args.observed else 1e-2
 
 mint = [1.29,1.94,2.2,2.5,3,3.8,4,4.4,4.7,5,5.17,5.5,6.05,6.4,6.7,7.2,7.7,8,8.3,
         8.6,9,9.4,9.8,10,10.6,10.8,11,11.5,11.9,12.1,12.4,12.8,13.3,13.7]
@@ -22,55 +30,63 @@ for i in np.arange(len(mint)):
             if mint[i]<Data[sim][t]['time']<maxt[i]:
                 for h in Data[sim][t]['halos']:
                     halo = Data[sim][t]['halos'][h]
-                    if halo['Mstar'] == 0:
-                        Plots['dark'][-1].append((halo['Mstar']+halo['Mgas'])/halo['Mvir'])
-                        Plots['dark_Inner'][-1].append(float((halo['Mstar_Inner']+halo['Mgas_Inner'])/halo['Mvir_Inner']))
+                    if args.observed:
+                        if halo['Mstar'] == 0:
+                            Plots['dark'][-1].append((.6*halo['Mstar']+1.33*halo['MHI'])/halo['Mvir'])
+                            Plots['dark_Inner'][-1].append(float((.6*halo['Mstar_Inner']+1.33*halo['MHI_Inner'])/halo['Mvir_Inner']))
+                        else:
+                            Plots['luminous'][-1].append((.6*halo['Mstar']+1.33*halo['MHI'])/halo['Mvir'])
+                            Plots['luminous_Inner'][-1].append(float((.6*halo['Mstar_Inner']+1.33*halo['MHI_Inner'])/halo['Mvir_Inner']))
                     else:
-                        Plots['luminous'][-1].append((halo['Mstar']+halo['Mgas'])/halo['Mvir'])
-                        Plots['luminous_Inner'][-1].append(float((halo['Mstar_Inner']+halo['Mgas_Inner'])/halo['Mvir_Inner']))
+                        if halo['Mstar'] == 0:
+                            Plots['dark'][-1].append((halo['Mstar']+halo['Mgas'])/halo['Mvir'])
+                            Plots['dark_Inner'][-1].append(float((halo['Mstar_Inner']+halo['Mgas_Inner'])/halo['Mvir_Inner']))
+                        else:
+                            Plots['luminous'][-1].append((halo['Mstar']+halo['Mgas'])/halo['Mvir'])
+                            Plots['luminous_Inner'][-1].append(float((halo['Mstar_Inner']+halo['Mgas_Inner'])/halo['Mvir_Inner']))
 
 for i in np.flip(np.arange(len(Plots['times']))):
     f,ax = plt.subplots(1,1,figsize=(11,8))
     ax.set_ylabel('N',fontsize=25)
-    ax.set_xlabel(r'f$_{b}$ within Rvir',fontsize=25)
+    ax.set_xlabel(pre+r' f$_{b}$ within Rvir',fontsize=25)
     ax.tick_params(length=5,labelsize=15)
-    ax.set_xlim([1e-7,1])
+    ax.set_xlim([xlower,1])
     ax.semilogx()
     ax.set_ylim([6e-1,2e3])
     ax.semilogy()
-    bins = np.logspace(-7,1,50)
+    bins = np.logspace(-11,1,80) if args.observed else np.logspace(-7,1,50)
     ax.plot(0,0,color='navy',linewidth=2,label='Luminous Halos')
     ax.plot(0,0,color='darkred',linewidth=2,label='Dark Halos')
-    ax.text(1e-2,1e3,f'Time: {round(Plots["times"][i],2)} Gyr',fontsize=25)
+    ax.text(tlower,1e3,f'Time: {round(Plots["times"][i],2)} Gyr',fontsize=25)
     ax.hist(Plots['dark'][i],bins,histtype='step',color='darkred',linewidth=3)
     ax.hist(Plots['dark'][i],bins,color='lightcoral',alpha=.7)
     ax.hist(Plots['luminous'][i],bins,histtype='step',color='navy',linewidth=3)
     ax.hist(Plots['luminous'][i],bins,color='cornflowerblue',alpha=.7)
     ax.legend(loc='upper left',prop={'size':15})
-    f.savefig(f'../GifPlots/Entire.{int(Plots["times"][i]*100):04d}.png',bbox_inches='tight',pad_inches=.1)
-    meta = OSXMetaData(f'../GifPlots/Entire.{int(Plots["times"][i]*100):04d}.png')
+    f.savefig(f'../GifPlots/{pre}Entire.{int(Plots["times"][i]*100):04d}.png',bbox_inches='tight',pad_inches=.1)
+    meta = OSXMetaData(f'../GifPlots/{pre}Entire.{int(Plots["times"][i]*100):04d}.png')
     meta.creator='Plot.py'
     plt.close()
 
     f,ax = plt.subplots(1,1,figsize=(11,8))
     ax.set_ylabel('N',fontsize=25)
-    ax.set_xlabel(r'f$_{b}$ within .1*Rvir',fontsize=25)
+    ax.set_xlabel(pre+r' f$_{b}$ within .1*Rvir',fontsize=25)
     ax.tick_params(length=5,labelsize=15)
-    ax.set_xlim([1e-7,1])
+    ax.set_xlim([xlower,1])
     ax.semilogx()
     ax.set_ylim([6e-1,2e3])
     ax.semilogy()
-    bins = np.logspace(-7,1,50)
+    bins = np.logspace(-11,1,80) if args.observed else np.logspace(-7,1,50)
     ax.plot(0,0,color='navy',linewidth=2,label='Luminous Halos')
     ax.plot(0,0,color='darkred',linewidth=2,label='Dark Halos')
-    ax.text(1e-2,1e3,f'Time: {round(Plots["times"][i],2)} Gyr',fontsize=25)
+    ax.text(tlower,1e3,f'Time: {round(Plots["times"][i],2)} Gyr',fontsize=25)
     ax.hist(Plots['dark_Inner'][i],bins,histtype='step',color='darkred',linewidth=3)
     ax.hist(Plots['dark_Inner'][i],bins,color='lightcoral',alpha=.7)
     ax.hist(Plots['luminous_Inner'][i],bins,histtype='step',color='navy',linewidth=3)
     ax.hist(Plots['luminous_Inner'][i],bins,color='cornflowerblue',alpha=.7)
     ax.legend(loc='upper left',prop={'size':15})
-    f.savefig(f'../GifPlots/Inner.{int(Plots["times"][i]*100):04d}.png',bbox_inches='tight',pad_inches=.1)
-    meta = OSXMetaData(f'../GifPlots/Inner.{int(Plots["times"][i]*100):04d}.png')
+    f.savefig(f'../GifPlots/{pre}Inner.{int(Plots["times"][i]*100):04d}.png',bbox_inches='tight',pad_inches=.1)
+    meta = OSXMetaData(f'../GifPlots/{pre}Inner.{int(Plots["times"][i]*100):04d}.png')
     meta.creator='Plot.py'
     plt.close()
 
@@ -78,13 +94,13 @@ imagenames = os.listdir('../GifPlots/')
 imagenames = np.sort(np.array(imagenames))
 images,images_inner = [[],[]]
 for name in imagenames:
-    if name.split('.')[0]=='Entire':
+    if name.split('.')[0]==f'{pre}Entire':
         images.append(imageio.imread(f'../GifPlots/{name}'))
-    elif name.split('.')[0]=='Inner':
+    elif name.split('.')[0]==f'{pre}Inner':
         images_inner.append(imageio.imread(f'../GifPlots/{name}'))
-imageio.mimsave('../Plots/EntireBaryonicFraction.gif', images, duration=.15)
-meta = OSXMetaData('../Plots/EntireBaryonicFraction.gif')
+imageio.mimsave(f'../Plots/{pre}EntireBaryonicFraction.gif', images, duration=.15)
+meta = OSXMetaData(f'../Plots/{pre}EntireBaryonicFraction.gif')
 meta.creator='Plot.py'
-imageio.mimsave('../Plots/InnerBaryonicFraction.gif', images_inner, duration=.15)
-meta = OSXMetaData('../Plots/InnerBaryonicFraction.gif')
+imageio.mimsave(f'../Plots/{pre}InnerBaryonicFraction.gif', images_inner, duration=.15)
+meta = OSXMetaData(f'../Plots/{pre}InnerBaryonicFraction.gif')
 meta.creator='Plot.py'
