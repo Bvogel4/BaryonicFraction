001bec4b4390eab02035a0c8dc430895cbc6a4c8
jdvannest (glaurung) <jordanvannest@gmail.com>
2022-05-25 14:40:45 -0500

    Corrected MergerHistories script and added data for Marvel

--- Code/MergerHistories.Marvel.py
+++ Code/MergerHistories.Marvel.py
@@ -1,4 +1,5 @@
-import argparse,os,pickle,sys
+import argparse,os,pickle,sys,warnings
+warnings.filterwarnings('ignore')
 def myprint(string,clear=False):
     if clear:
         sys.stdout.write("\033[F")
@@ -12,12 +13,14 @@ args = parser.parse_args()
 
 try:
     Data = pickle.load(open('../DataFiles/MergerHistories.Marvel.pickle','rb'))
-    print('DataFile Loaded')
+    overwrite = False
 except:
     overwrite = True
 if overwrite or args.overwrite:
     print('Creating New DataFile')
     Data = {'cptmarvel':{},'elektra':{},'storm':{},'rogue':{}}
+else:
+    print('DataFile Loaded')
 
 DBs = {
     'cptmarvel':'/myhome2/users/munshi/dwarf_volumes/cptmarvel.cosmo25cmb.4096g5HbwK1BH/',
@@ -27,9 +30,8 @@ DBs = {
 }
 
 print('Loading Database...')
-os.environ['TANGOS_DB_CONNECTION'] = DBs[args.simulation]+f'{args.simulation}.working.db'
-from __future__ import absolute_import
-import tangos as db
+os.environ['TANGOS_DB_CONNECTION'] = f'{DBs[args.simulation]}{args.simulation}.working.db'
+import tangos
 import tangos.relation_finding
 import numpy as np
 
@@ -45,7 +47,7 @@ def get_mergers_of_major_progenitor(input_halo):
     ratio = []
     halo  = []
     while input_halo is not None:
-        mergers = db.relation_finding.MultiHopMostRecentMergerStrategy(input_halo, order_by='weight').all()
+        mergers = tangos.relation_finding.MultiHopMostRecentMergerStrategy(input_halo, order_by='weight').all()
         if len(mergers)>0 :
             for m in mergers[1:]:
                 times.append(mergers[0].timestep.next.time_gyr)
@@ -57,7 +59,7 @@ def get_mergers_of_major_progenitor(input_halo):
 
     return np.array(times), np.array(ratio), halo
 
-sim = tangos.get_simulation(DBs[args.simulation].split('/')[-1])[-1]
+sim = tangos.get_simulation(DBs[args.simulation].split('/')[-2])[-1]
 hid,grp,ngas = sim.calculate_all('halo_number()','Grp','N_gas')
 hid = hid[ngas>0]
 grp = grp[ngas>0]
@@ -68,11 +70,12 @@ myprint('Database Loaded',clear=True)
 print('Writing: 0.00%')
 prog=0
 for i in np.arange(len(hid)):
-    t,r,h = get_mergers_of_major_progenitor(sim[hid[i]])
+    t,r,h = get_mergers_of_major_progenitor(sim[int(hid[i])])
     for j in np.arange(len(t)):
         Data[args.simulation][str(grp[i])]['times'].append(t[j])
         Data[args.simulation][str(grp[i])]['ratios'].append(r[j])
         Data[args.simulation][str(grp[i])]['halos'].append((h[j][0]['Grp'],h[j][1]['Grp']))
+    prog+=1
     myprint(f'Writing: {round(prog/len(hid)*100,2)}%',clear=True)
 
 out = open('../DataFiles/MergerHistories.Marvel.pickle','wb')
